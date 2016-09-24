12a66bd56cbcd2e8ff9d68fce12314362eba7aa7
flyleaf91 <flyleaf91@163.com>
2016-09-24 11:36:55 +0800

    添加液晶例程（霸道版本）

---
+++ 28-液晶显示/2-程序/2-液晶坐标方向演示/Project/RVMDK（uv4）/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'BH-F103' 
+ * Target:  'œ‘ æ”¢Œƒ' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ 28-液晶显示/2-程序/2-液晶坐标方向演示/User/lcd/bsp_lcd.c
@@ -0,0 +1,18 @@
+#include "bsp_lcd.h"
+
+
+
+/**
+  * @brief  LCD 初始化函数
+  * @param  无
+  * @retval 无
+  */
+void LCD_Init ( void )
+{
+	ILI9341_Init ();
+	
+
+	
+}
+
+
