6717b6760e47440df7547af234b964c4ab9c1c79
flyleaf91 <flyleaf91@163.com>
2016-10-29 16:02:51 +0800

    修改sct文件例程

---
+++ MDK编译过程及文件全解/程序/SCT文件应用/1.自动分配变量到指定空间/Project/RVMDK（uv4）/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'F103-指南者-STM32' 
+ * Target:  '外部SRAM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
