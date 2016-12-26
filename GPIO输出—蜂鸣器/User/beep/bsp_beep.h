8ecfb23166ca06d3a1c7ff599f716c90dc8bce7d
flyleaf91 <flyleaf91@163.com>
2016-12-26 15:09:21 +0800

    测试蜂鸣器例程

--- GPIO输出—蜂鸣器/User/beep/bsp_beep.h
+++ GPIO输出—蜂鸣器/User/beep/bsp_beep.h
@@ -6,9 +6,9 @@
 
 
 /* 定义蜂鸣器连接的GPIO端口, 用户只需要修改下面的代码即可改变控制的蜂鸣器引脚 */
-#define BEEP_GPIO_PORT    	GPIOC			              /* GPIO端口 */
-#define BEEP_GPIO_CLK 	    RCC_APB2Periph_GPIOC		/* GPIO端口时钟 */
-#define BEEP_GPIO_PIN		  GPIO_Pin_0			        /* 连接到蜂鸣器的GPIO */
+#define BEEP_GPIO_PORT    	GPIOA			              /* GPIO端口 */
+#define BEEP_GPIO_CLK 	    RCC_APB2Periph_GPIOA		/* GPIO端口时钟 */
+#define BEEP_GPIO_PIN		  GPIO_Pin_8			        /* 连接到蜂鸣器的GPIO */
 
 /* 高电平时，蜂鸣器响 */
 #define ON  1
